bbc3ad6aa0c098e1f34c48fe1d13acc2713dbbea
AronAland <aron.aland@khk.ee>
2020-05-29 18:40:52 +0300

    Apache2 paigaldusskript

--- apache_paigaldamine.sh
+++ apache_paigaldamine.sh
@@ -7,6 +7,8 @@ if [ $apache_olemas -eq 0 ]; then
 	echo "Apache2 on paigaldatud"
 elif [ $apache_olemas -eq 1 ]; then
 	echo "Apache2 on juba paigaldatud!"
+	service apache2 start
+	service apache2 status
 fi
 
 # skripti lõpp
